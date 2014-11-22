2750175cbbfa21f0d20c986ed6f9e4d5be76bf3f
fras2560 <fras2560@mylaurier.ca>
2014-11-22 13:22:52 -0500

    Basw test class

---
+++ src/BaseTest.hpp
@@ -0,0 +1,36 @@
+/*
+ -------------------------------------------------------
+ Author: Tom LaMantia
+ Id: 110283790
+ Email: lama3790@mylaurier.ca
+ Version: December 1, 2014
+ -------------------------------------------------------
+ */
+
+/*
+   -------------------------------------------------------
+    This header file contains the definition for the Test class
+
+    Implementations of these methods are contained in Test.cpp
+    -------------------------------------------------------
+ */
+
+#ifndef BASETEST_HPP_
+#define BASETEST_HPP_
+#include"ConveyorBelt.hpp"
+#include"GameArea.hpp"
+#include<stdio.h>
+class BaseTest{
+
+public:
+	BaseTest();
+	virtual ~BaseTest();
+	void assert(bool, bool, char*);
+	virtual void setUp();
+	virtual void tearDown();
+};
+
+
+
+
+#endif /* TEST_HPP_ */
